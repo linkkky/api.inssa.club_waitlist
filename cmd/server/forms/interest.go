4794fe17b2de2f05ab04cc6f4fa2cb03aabb4d93
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-17 18:10:15 +0900

    feat(forms): Add struct AddInterest

--- cmd/server/forms/interest.go
+++ cmd/server/forms/interest.go
@@ -1,1 +1,7 @@
 package forms
+
+// AddInterest is a struct for adding interest request
+type AddInterest struct {
+	UserID string `json:"user_id" validate:"optional"`
+	Email  string `json:"email" validate:"email, required"`
+}
