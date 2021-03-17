96a4ab63afcc1e1ad652666e0bf66012dd08338a
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-17 17:20:19 +0900

    feat(models): Add example data to struct Interest

--- cmd/server/models/interest.go
+++ cmd/server/models/interest.go
@@ -7,7 +7,9 @@ import (
 // Interest is a model, which is the entity of waitlist
 type Interest struct {
 	gorm.Model
-	ID              uint64 `gorm:"primary_key"`
-	ClubhouseUserID uint64 `gorm:"column:clubhouse_user_id;unique;"`
-	Email           string `gorm:"column:email;unique;not null" valid:"email"`
+	ID              uint64 `gorm:"primary_key" example:"3"`
+	ClubhouseUserID uint64 `gorm:"column:clubhouse_user_id;unique;" example:"123456"`
+	Email           string `gorm:"column:email;unique;not null" valid:"email" example:"example@example.com"`
+}
+
 }
