bf12b1a3ab3b18333ffa02309dc377d1f43e02f6
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-18 01:14:30 +0900

    feat(controllers): Update function AddInterest to return struct AddInterestResponse

--- cmd/server/controllers/interest.go
+++ cmd/server/controllers/interest.go
@@ -27,22 +27,24 @@ func (ctrler *Controller) AddInterest(c *gin.Context) {
 	// validators
 
 	db.Model(&models.Interest{}).Where("email = ?", form.Email).Count(&count)
-
 	if count > 0 {
 		utils.AbortWithErrorResponse(c, http.StatusBadRequest, errors.DuplicateEmailError, "")
 		return
 	}
+	// check unique
 
 	interest := models.Interest{ClubhouseUserID: form.ClubhouseUserID, Email: form.Email}
 	if err := interest.Create(db); err != nil {
 		utils.AbortWithErrorResponse(c, http.StatusBadRequest, errors.DuplicateEmailError, err.Error())
 	}
+	response := forms.AddInterestResponse{ID: interest.ID, ClubhouseUserID: interest.ClubhouseUserID, Email: interest.Email}
 
-	c.JSON(http.StatusCreated, interest)
+	c.JSON(http.StatusCreated, response)
 }
 
 func (ctrl *Controller) DeleteInterest(c *gin.Context) {
 	var form forms.DeleteInterest
+	var count int64
 
 	db := utils.GetDB().Instance
 	if err := c.ShouldBindJSON(&form); err != nil {
