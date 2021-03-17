392308178e4e0b0edc7f27a5c7bdc6a72154ac17
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-17 23:11:42 +0900

    refactor(forms): Replace struct AddInterest to AddInterestRequest

--- cmd/server/forms/interest.go
+++ cmd/server/forms/interest.go
@@ -2,8 +2,11 @@ package forms
 
 import "gopkg.in/guregu/null.v4"
 
-// AddInterest is a struct for adding interest request
-type AddInterest struct {
+// AddInterestRequest is a struct for adding interest request
+type AddInterestRequest struct {
+	ClubhouseUserID null.Int `json:"clubhouse_user_id" valid:"optional"`
+	Email           string   `json:"email" valid:"email, required" binding:"required"`
+}
 	ClubhouseUserID null.Int `json:"clubhouse_user_id" valid:"optional"`
 	Email           string   `json:"email" valid:"email, required" binding:"required"`
 }
