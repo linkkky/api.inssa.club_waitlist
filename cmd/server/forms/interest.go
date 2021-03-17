ce02c6b159e2d70aa3dd37600f561c219758cc86
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-17 18:10:37 +0900

    feat(forms): Add struct DeleteInterest

--- cmd/server/forms/interest.go
+++ cmd/server/forms/interest.go
@@ -5,3 +5,8 @@ type AddInterest struct {
 	UserID string `json:"user_id" validate:"optional"`
 	Email  string `json:"email" validate:"email, required"`
 }
+
+// DeleteInterest is a struct for deleting interest request
+type DeleteInterest struct {
+	Email string `json:"email" validate:"email, required"`
+}
