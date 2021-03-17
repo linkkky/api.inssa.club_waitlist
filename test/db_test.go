c70d7cf4faebbd88a35e2bb7c8d581c51c722189
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-17 14:15:32 +0900

    test: Add test for creating record in DB

--- test/db_test.go
+++ test/db_test.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"inssa_club_waitlist_backend/cmd/server/models"
 	"inssa_club_waitlist_backend/cmd/server/utils"
 	"testing"
 
@@ -19,4 +20,21 @@ func TestDBSetup(t *testing.T) {
 	db := utils.GetDB()
 	db.SetupDB()
 	db.Instance.Exec("DELETE FROM interests") // reset db data
+}
+
+func TestRecordCreation(t *testing.T) {
+	db := utils.GetDB()
+	db.SetupDB()
+	const CLUBHOUSE_USER_ID = 1234
+	const EMAIL = "code.yeon.gyu@gmail.com"
+
+	interest := models.Interest{ClubhouseUserID: CLUBHOUSE_USER_ID, Email: EMAIL}
+	db.Instance.Create(&interest)
+
+	result := models.Interest{}
+	db.Instance.Where(models.Interest{ClubhouseUserID: CLUBHOUSE_USER_ID}).First(&result)
+	// retrieve the same data
+
+	assert.Equal(t, true, interest.Email == result.Email)
+	// check if the requested data and created data is the same
 }
