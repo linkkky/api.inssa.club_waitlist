6c9819bf7f8e9ff10cdedf7c5a728fa50909e706
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-17 20:46:18 +0900

    fix(forms): Typo

--- cmd/server/forms/interest.go
+++ cmd/server/forms/interest.go
@@ -2,11 +2,11 @@ package forms
 
 // AddInterest is a struct for adding interest request
 type AddInterest struct {
-	UserID string `json:"user_id" validate:"optional"`
-	Email  string `json:"email" validate:"email, required"`
+	UserID string `json:"user_id" valid:"optional"`
+	Email  string `json:"email" valid:"email, required" binding:"required"`
 }
 
 // DeleteInterest is a struct for deleting interest request
 type DeleteInterest struct {
-	Email string `json:"email" validate:"email, required"`
+	Email string `json:"email" valid:"email, required" binding:"required"`
 }
