21b518e129a4916719234fbb9ed4f7b6d7439fc1
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-17 20:47:25 +0900

    fix(models): Add missing validator

--- cmd/server/models/interest.go
+++ cmd/server/models/interest.go
@@ -10,7 +10,7 @@ type Interest struct {
 	gorm.Model
 	ID              uint64 `gorm:"primary_key" example:"3"`
 	ClubhouseUserID uint64 `gorm:"column:clubhouse_user_id;unique;" example:"123456"`
-	Email           string `gorm:"column:email;unique;not null" valid:"email" example:"example@example.com"`
+	Email           string `gorm:"column:email;unique;not null" valid:"email, required" example:"example@example.com"`
 }
 
 func (interest *Interest) Create(db *gorm.DB) error {
