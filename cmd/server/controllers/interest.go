3c7c58ba1a280f34facc977e02acecb28924f377
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-17 21:41:47 +0900

    feat(controllers): Add function AddInterest

--- cmd/server/controllers/interest.go
+++ cmd/server/controllers/interest.go
@@ -1,1 +1,42 @@
 package controllers
+
+import (
+	"inssa_club_waitlist_backend/cmd/server/errors"
+	"inssa_club_waitlist_backend/cmd/server/forms"
+	"inssa_club_waitlist_backend/cmd/server/models"
+	"inssa_club_waitlist_backend/cmd/server/utils"
+	"net/http"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gin-gonic/gin"
+)
+
+func (ctrler *Controller) AddInterest(c *gin.Context) {
+	var form forms.AddInterest
+	var count int64
+	db := utils.GetDB().Instance
+
+	if err := c.ShouldBindJSON(&form); err != nil {
+		utils.AbortWithErrorResponse(c, http.StatusBadRequest, errors.ValidationError, err.Error())
+		return
+	}
+	if ok, err := govalidator.ValidateStruct(form); !ok {
+		utils.AbortWithErrorResponse(c, http.StatusBadRequest, errors.ValidationError, err.Error())
+		return
+	}
+	// validators
+
+	db.Model(&models.Interest{}).Where("email = ?", form.Email).Count(&count)
+
+	if count > 0 {
+		utils.AbortWithErrorResponse(c, http.StatusBadRequest, errors.DuplicateEmailError, "")
+		return
+	}
+
+	interest := models.Interest{ClubhouseUserID: form.ClubhouseUserID, Email: form.Email}
+	if err := interest.Create(db); err != nil {
+		utils.AbortWithErrorResponse(c, http.StatusBadRequest, errors.DuplicateEmailError, err.Error())
+	}
+
+	c.JSON(http.StatusCreated, interest)
+}
