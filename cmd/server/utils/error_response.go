6d3fc5d3b448919da1c5c5bcc8e8a43b1c014136
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-17 18:09:23 +0900

    feat(utils): Add function AbortWithErrorResponse

--- cmd/server/utils/error_response.go
+++ cmd/server/utils/error_response.go
@@ -1,1 +1,12 @@
 package utils
+
+import (
+	"inssa_club_waitlist_backend/cmd/server/errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+// AbortWithErrorResponse aborts the request with the given error
+func AbortWithErrorResponse(c *gin.Context, statusCode int, errorType string, detail string) {
+	c.AbortWithStatusJSON(statusCode, gin.H{"errorType": errorType, "message": errors.Messages[errorType], "detail": detail})
+}
