a9f1c899977380274f5354bb9da576d3e0b059ce
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-17 15:28:26 +0900

    feat(models): Update model interest to use default gorm model fields

--- cmd/server/models/interest.go
+++ cmd/server/models/interest.go
@@ -1,16 +1,13 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 // Interest is a model, which is the entity of waitlist
 type Interest struct {
-	ID              uint64         `gorm:"primary_key"`
-	ClubhouseUserID uint64         `gorm:"column:clubhouse_user_id;unique;"`
-	Email           string         `gorm:"column:email;unique;not null"`
-	CreatedAt       time.Time      `gorm:"column:created_at;autoCreateTime"`
-	DeletedAt       gorm.DeletedAt `gorm:"column:delete_at"`
+	gorm.Model
+	ID              uint64 `gorm:"primary_key"`
+	ClubhouseUserID uint64 `gorm:"column:clubhouse_user_id;unique;"`
+	Email           string `gorm:"column:email;unique;not null" valid:"email"`
 }
