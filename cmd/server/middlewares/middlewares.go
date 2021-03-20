63bbf9f2b3e36eb4d5e6ab41107726c10707f8da
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-03-20 09:12:20 +0900

    feat(middlewares): Add cors setup

--- cmd/server/middlewares/middlewares.go
+++ cmd/server/middlewares/middlewares.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	ginlogrus "github.com/toorop/gin-logrus"
@@ -11,4 +12,5 @@ func Setup(engine *gin.Engine) {
 	logger := logrus.New()
 	engine.Use(gin.Recovery())
 	engine.Use(ginlogrus.Logger(logger))
+	engine.Use(cors.Default())
 }
